3d6c92f378bde7cdbe2f52d29fe434b9e77552fe
dkippes <kippes.diego@gmail.com>
2021-01-24 03:29:36 -0300

    props de react en componentes

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -16,48 +16,55 @@ REGLAS JSX:
   -formatting
 */
 
+// SETUP VARS
+const firstBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg',
+  title: '1984',
+  author: 'George Orwell',
+};
+
+const secondBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/716zRCJKTFL._AC_UL200_SR200,200_.jpg',
+  title: 'The Hill We Climb: An Inaugural Poem for the Country',
+  author: 'Amanda Gorman',
+};
+
 //Creando mi componente (se necesita capitalizar el nombre, es una funcion especial / se puede usar arrow function)
 function BookList() {
   //De aca devuelve "html" - se llama en realidad "jsx"
   //Puedo usar -> React.createElement('h1', {}, 'hello world');
   //Puedo usar <React.Fragment></..> porque solo puedo retornar un solo elemento
   return (
-    <section className='bookList'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="bookList">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        
+      </Book>
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const {img, title, author} = props;
   return (
-    <article className='book'>
-      <Image></Image>
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src='https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL._AC_UL200_SR200,200_.jpg'
-    alt=''
-  />
-);
-
-const Title = () => <h1>1984</h1>;
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25' }}>
-    George Orwell
-  </h4>
-);
 
 //Renderizo mi componente en index.html (se necesita cerrar el componente con tags "<Greeting><Greeting/>")
 ReactDom.render(<BookList />, document.getElementById('root'));
