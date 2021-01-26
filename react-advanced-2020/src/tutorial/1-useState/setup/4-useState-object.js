acc463b11a6c6cfbe254122528257a2f3e33e9f2
dkippes <kippes.diego@gmail.com>
2021-01-26 20:35:08 -0300

    useState-objects

--- react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'diego', 
+    age: 24, 
+    message: 'hola soy un mensaje'
+  });
+
+  /* 
+  DE ESTA FORMA NO ESTAMOS USANDO OBJETOS
+  const [name, setName] = useState('diego')
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('este'); */
+
+
+  const changeMessage = () => {
+    setPerson({...person, message: 'Este es un nuevo mensaje'}); //Al usar spread operator no borramos la persona y cambiamos el msg
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>Change Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
