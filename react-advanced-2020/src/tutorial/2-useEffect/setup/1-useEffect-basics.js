4835f300a77502a59947afa3837c81dbd5e7a24f
dkippes <kippes.diego@gmail.com>
2021-01-27 05:43:53 -0300

    useEffect-basics

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,36 @@
 import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// second parameter - array of dependacies
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  //Se usa para sideEffects
+  useEffect(()=>{
+    console.log('use effect');
+
+    if (value >= 1) {
+      document.title = `Mensajes(${value})`;
+    } else {
+      document.title = `No hay Mensajes`;
+    }
+  }, [value]); //Si le pasamos el segundo parametro va a cambiar Solo si el value cambia
+
+  //Si se mira el log, este corre 1 sola vez pero como en el array no esta el value a cambiar este no hace render
+  useEffect(() => {
+    console.log('hello world');
+  }, [])
+
+  console.log('render');
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>Mas mensajes</button>
+      <button className="btn" onClick={() => setValue(value - 1)}>Menos Mensajes</button>
+    </>
+  )
 };
 
 export default UseEffectBasics;
