ea36d1654d3dbc2dacfbf073a9eb6483436861df
dkippes <kippes.diego@gmail.com>
2021-01-28 04:14:11 -0300

    short circuit

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,18 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('kippes');
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+  return <>
 
-  return <h2>short circuit</h2>;
+    {/* <h1>{firstValue}</h1>
+    <h1>value : {secondValue}</h1> */}
+
+    {/* {if(){console.log('ah')}} //Tiraria un error */} 
+    <h1>Or: {text || 'diego'}</h1>
+    <h2>And: {text && 'diego'}</h2>
+  </>;
 };
 
 export default ShortCircuit;
