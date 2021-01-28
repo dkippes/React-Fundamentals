50f7b594ba78f79ffa1ec2a0f2679ae579752a49
dkippes <kippes.diego@gmail.com>
2021-01-28 04:47:17 -0300

    input forms e.prevent

--- react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -7,8 +7,11 @@ import React, { useState } from 'react';
 
 const ControlledInputs = () => {
 
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+
   const handleSubmit = (e) => {
-    alert('hello world')
+    alert("Nombre: " + firstName + " con email: " + email)
     e.preventDefault();
     
   }
@@ -19,11 +22,27 @@ const ControlledInputs = () => {
         <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name: </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <button type="submit">Add Person</button>
         </form>
