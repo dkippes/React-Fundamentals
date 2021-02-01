4d9a3543bfce66e59805013a9a98f62f5f82eafc
dkippes <kippes.diego@gmail.com>
2021-01-31 21:07:47 -0300

    useRef

--- react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,36 @@
 import React, { useEffect, useRef } from 'react';
 
+// It works like useState
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+  }
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+  
+  return (
+    <>
+      <form className="form" onClick={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <div ref={divContainer}>Hell World</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
